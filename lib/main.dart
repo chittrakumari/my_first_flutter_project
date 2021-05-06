ef23cda3b7cee7a5e5532adea83c5cead44d466a
Chittra <chittra.kumari_cs.ccv19@gla.ac.in>
2021-05-06 13:59:34 +0530

    Scaffold Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,22 @@
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-  home: Home()
+//Scaffold widget helps us to build the basic layout of our app .We can add the appbar,icons, buttons etc.
+void main()=>runApp(MaterialApp(
+  home:Scaffold(
+    appBar: AppBar(
+      title: Text(
+        'Helloa!'
+      ),
+      centerTitle: true,
+
+    ),
+    body: Center(
+child:Text('Stay Safe Everyone'),
+
+),
+    floatingActionButton: FloatingActionButton(
+      child: Text('Click '),
+    ),
+),
 )
 );
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-
-        appBar: AppBar(
-          title: Text('Hello World',style:
-          TextStyle(
-              fontSize: 40.0,
-              fontWeight: FontWeight.bold
-
-          ),
-          ),
-          centerTitle : true,
-
-          backgroundColor: Colors.blue[600],
-        ),
-
-
-        body:Container(
-          //Padding is the inside in the container
-          padding: EdgeInsets.fromLTRB(10.0,20.0,30.0,40.0),
-
-          //Marging is outside the container.
-    margin: EdgeInsets.all(30.0),
-
-          color: Colors.amber,
-          child: Text('Stay Safe',
-          style: TextStyle(
-              fontSize: 50.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OrelegaOne',
-          )
-          ),
-        ),
-
-
-
-
-
-        floatingActionButton: FloatingActionButton(
-          //Annoynomos function
-          onPressed:() {
-
-          },
-          backgroundColor: Colors.red[600],
-          child:Text('Click'),
-
-        )
-
-
-    );
-  }
-}
-
-
